a575eba6373383b9b81a5821fa237902b4334e07
Vikas Joshi <vikasjoshi10889@gmail.com>
2024-10-23 23:30:38 +0530

    Updated

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ let btn=document.querySelector("button");
 let inp=document.querySelector("input");
 let ul=document.querySelector("ul");
 
-btn.addEventListener("click", function () {
+
+ btn.addEventListener("click", (addtask) => {
     let item=document.createElement("li");
     item.innerText=inp.value;
-    ul.appendChild(item);
-    inp.value="";
+
+    function empty() {
+        for(let i=0; i<inp.value; i++){
+            inp.value=" ";
+        }
+        if(inp.value) {
+            ul.appendChild(item);
+        }else {
+            ul.removeChild(item);
+        }
+    }
+    empty();
 
     let delbtn=document.createElement("button");
+    delbtn.style.color="red";
     delbtn.innerText="Delete";
     delbtn.classList.add("Delete");
-
     item.appendChild(delbtn);
 });
 
@@ -20,6 +31,6 @@ ul.addEventListener("click", function (event) {
     if(event.target.nodeName=="BUTTON") {
         let listitem=event.target.parentElement;
         listitem.remove();
-        console.log(listitem,"deleted");
+        console.dir(listitem,"deleted");
     }
 });
